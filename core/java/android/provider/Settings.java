2b4fa3c46f96aa93ea0594a599689d6c7415444d
htchoi1995 <htchoi1995@gmail.com>
2013-09-26 13:01:58 +0200

    [1/2] SystemUI: Network speed indicator
    Change-Id: I88e7cdfe20342c695a33383db36bf80bb4ffe009
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3734,6 +3734,13 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
+
+        /* Network speed indicator
+         * @hide
+         */
+        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
