696d0a8963c070dd4f47456b773d91cd3b76deed
Tom Marshall <tdm@cyngn.com>
2014-06-03 12:38:06 -0700

    Handle NPE due to threading race
    Change-Id: I6eab5bc3d687edfa6f0839e439b3314bed4fdcea

--- media/java/android/media/MediaPlayer.java
+++ media/java/android/media/MediaPlayer.java
@@ -2852,6 +2852,7 @@ public class MediaPlayer extends PlayerBase
                 Log.w(TAG, "mediaplayer went away with unhandled events");
                 return;
             }
+            try {
             switch(msg.what) {
             case MEDIA_PREPARED:
                 try {
@@ -3029,6 +3030,13 @@ public class MediaPlayer extends PlayerBase
             default:
                 Log.e(TAG, "Unknown message type " + msg.what);
                 return;
+            }
+            } catch (NullPointerException e) {
+                /**
+                 * We may get an NPE even with the null checks above due
+                 * to threading issues.  Just ignore it.
+                 */
+                Log.e(TAG, "Unhandled NPE from message type " + msg.what);
             }
         }
     }
