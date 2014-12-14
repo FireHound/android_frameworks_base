d9e23c40f81b5a9f3eb1110042cc0fd02663117f
0xD34D <clark@scheffsblend.com>
2014-12-14 21:23:05 +0100

    FWB: Gesture Anywhere [1/2]
    Change-Id: I04acb9a88fe0011921a1bafdc1919d76ce57aedf
    
    Conflicts:
    	core/java/android/app/IBatteryService.aidl
    	packages/SettingsLib/res/values-sr/cm_strings.xml
    	packages/SystemUI/res/values/custom_colors.xml
    	packages/SystemUI/res/values/custom_config.xml
    	packages/SystemUI/res/values/rr_colors.xml
    	packages/SystemUI/res/values/rr_config.xml
    	packages/SystemUI/res/values/rr_dimens.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    
    Conflicts:
    	packages/SystemUI/res/values/fh_colors.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -19,6 +19,9 @@ package com.android.systemui.statusbar.phone;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.TimeInterpolator;
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
 import android.annotation.NonNull;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
@@ -882,6 +885,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // ================================================================================
     // Constructing the view
     // ================================================================================
+    @ChaosLab(name="GestureAnywhere", classification=Classification.CHANGE_CODE)
     protected PhoneStatusBarView makeStatusBarView() {
         final Context context = mContext;
 
@@ -977,9 +981,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             // no window manager? good luck with that
         }
 
+        addGestureAnywhereView();
+
         if (mAssistManager == null) {
             mAssistManager = new AssistManager(this, context);
         }
+
         if (mNavigationBarView == null) {
             mAssistManager.onConfigurationChanged();
         }
