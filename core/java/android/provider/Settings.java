671ab73f184fbe53adc36745f9772012c93a7830
Dave Kessler <activethrasher00@gmail.com>
2015-04-04 15:47:24 +0200

    SystemUI: Add membar to recents options [1/2]
    Change-Id: Ieaf86a2360cc993bb20f971b25c2b8c49a746f64

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4292,6 +4292,12 @@ public final class Settings {
         public static final String TOAST_ANIMATION = "toast_animation";
 
         /**
+          * show the membar in recents
+          *  @hide
+          */
+        public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
