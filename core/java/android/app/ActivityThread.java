41155d92073ac1c40032db8282b95149a2c467a6
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-06-18 20:08:56 +0200

    ActivityManagerNative: Prevent possible soft-reboot
    E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke interface method 'void android.app.IActivityManager.attachApplication(android.app.IApplicationThread)' on a null object reference
    E AndroidRuntime: 	at android.app.ActivityThread.attach(ActivityThread.java:5110)
    E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:5262)
    E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Method.java:375)
    E AndroidRuntime: 	at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:904)
    E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:699)
    E AndroidRuntime: Error reporting crash
    E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke interface method 'void android.app.IActivityManager.handleApplicationCrash(android.os.IBinder, android.app.ApplicationErrorReport$CrashInfo)' on a null object reference
    E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$UncaughtHandler.uncaughtException(RuntimeInit.java:89)
    E AndroidRuntime: 	at java.lang.ThreadGroup.uncaughtException(ThreadGroup.java:693)
    E AndroidRuntime: 	at java.lang.ThreadGroup.uncaughtException(ThreadGroup.java:690)
    
    Change-Id: I6940b723de455ae867ae784d4636e38a4196a689
    
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -5949,7 +5949,9 @@ public final class ActivityThread {
             RuntimeInit.setApplicationObject(mAppThread.asBinder());
             final IActivityManager mgr = ActivityManagerNative.getDefault();
             try {
-                mgr.attachApplication(mAppThread);
+                if (mgr != null) {
+                    mgr.attachApplication(mAppThread);
+                }
             } catch (RemoteException ex) {
                 throw ex.rethrowFromSystemServer();
             }
@@ -5968,7 +5970,9 @@ public final class ActivityThread {
                                 + " used=" + (dalvikUsed/1024));
                         mSomeActivitiesChanged = false;
                         try {
-                            mgr.releaseSomeActivities(mAppThread);
+                            if (mgr != null) {
+                                mgr.releaseSomeActivities(mAppThread);
+                            }
                         } catch (RemoteException e) {
                             throw e.rethrowFromSystemServer();
                         }
