b909be28af55bb2edff111cdf4e85df54f435cf3
Jmz <jmztaylor@gmail.com>
2015-10-29 23:16:34 +0000

    Allow disabling of force close dialogs [1/2]
    BenzoEdit: marshmallow->oreo
    Change-Id: I521669fbb54046ec7c7002a7d6a63eebfec80049

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4739,6 +4739,12 @@ public final class Settings {
         public static final String CARBON_CUSTOM_GESTURE_PACKAGE_DOWN = "carbon_custom_gesture_package_down";
 
         /**
+         * Disable force close notifications
+         * @hide
+         */
+        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
