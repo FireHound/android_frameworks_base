3a9366e9171f9c53a92b2242ed4a26f41ab87217
Jmz <jmztaylor@gmail.com>
2015-10-29 22:56:21 +0000

    Disable Immersive Messages [1/2]
    Change-Id: Ie5559d8bb50374b3a13afb6efd91fdfb9a77d179
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4574,6 +4574,12 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
 
         /**
+         * Disable Immersive Message
+         * @hide
+         */
+        public static final String DISABLE_IMMERSIVE_MESSAGE = "disable_immersive_message";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
