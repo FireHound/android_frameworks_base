ff696bae5610080697908ee39d03321f5af1fb3e
Jmz <jmztaylor@gmail.com>
2015-10-29 23:16:34 +0000

    Allow disabling of FC dialogs [1/2]
    Change-Id: Ic473c00ccf0c6c2e2a0c93ca2219210739b125ed

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4873,6 +4873,12 @@ public final class Settings {
         public static final String PULSE_AMBIENT_LIGHT_COLOR = "pulse_ambient_light_color";
 
         /**
+         * Disable FC Notifications
+         * @hide
+         */
+        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
