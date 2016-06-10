78434e3e173c7f62b26f03ef7a8eb30a11a25665
Willi Ye <williye97@gmail.com>
2016-06-10 14:41:59 +0200

    Solid battery: Bring back pulsing animation
    Change-Id: Id80ff1c126b272ed65f0170cb3def026f8cce0b9
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -281,6 +281,9 @@ public class BatteryMeterDrawable extends Drawable implements
 
         if (mStyle == BATTERY_STYLE_CIRCLE) {
             animateCircleBattery(level, pluggedIn, charging);
+        } else if (CMSettings.System.getInt(mContext.getContentResolver(),
+                CMSettings.System.STATUS_BAR_BATTERY_STYLE, 0) == 7) {
+            animateSolidBattery(level, pluggedIn, charging);
         }
 
         postInvalidate();
@@ -378,6 +381,37 @@ public class BatteryMeterDrawable extends Drawable implements
     }
 
     public void animateCircleBattery(int level, boolean pluggedIn, boolean charging) {
+        if (charging) {
+            if (mAnimator != null) mAnimator.cancel();
+
+            final int defaultAlpha = mLevelDrawable.getAlpha();
+            mAnimator = ValueAnimator.ofInt(defaultAlpha, 0, defaultAlpha);
+            mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    mLevelDrawable.setAlpha((int) animation.getAnimatedValue());
+                    invalidateSelf();
+                }
+            });
+            mAnimator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    mLevelDrawable.setAlpha(defaultAlpha);
+                    mAnimator = null;
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLevelDrawable.setAlpha(defaultAlpha);
+                    mAnimator = null;
+                }
+            });
+            mAnimator.setDuration(2000);
+            mAnimator.start();
+        }
+    }
+
+    public void animateSolidBattery(int level, boolean pluggedIn, boolean charging) {
         if (charging) {
             if (mAnimator != null) mAnimator.cancel();
 
