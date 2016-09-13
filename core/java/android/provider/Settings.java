cd58c4bed3db6d90439d2afe62ffb8e71845c5a3
Alex <mazdarider23@gmail.com>
2016-09-12 21:41:56 -0400

    Remove dashboard tile summaries [2/2]
    Change-Id: I028523a50949741d11e61d88cd50891404a52b26
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3719,13 +3719,10 @@ public final class Settings {
          */
 
         /**
-          * Volume keys control cursor in text fields (default is 0)
-          * 0 - Disabled
-          * 1 - Volume up/down moves cursor left/right
-          * 2 - Volume up/down moves cursor right/left
-          * @hide
-          */
-         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+         * Remove tile summaries in settings
+         * @hide
+         */
+        public static final String REMOVE_TILE_SUMMARY = "remove_tile_summary";
 
         /**
          * Toast icon
