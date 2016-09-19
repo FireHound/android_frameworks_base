93756e1a117b8cfdb54a6469df8ef45eb758603c
PMS22 <prathams99@rediff.com>
2016-09-19 13:13:18 +0000

    Removed derps

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3718,6 +3718,15 @@ public final class Settings {
          * the setting value. See an example above.
          */
 
+	/**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
         /**
          * Remove tile summaries in settings
          * @hide
