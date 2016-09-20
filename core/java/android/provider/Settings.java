a4b69817c0ce4358adb67a12c3720c1099df3bd3
PMS22 <prathams99@rediff.com>
2016-09-20 15:23:45 +0000

    Fix the derp!!!

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3725,6 +3725,15 @@ public final class Settings {
         public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
 
 	/**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+	/**
          * Disable dashboard conditions in settings
          * @hide
          */
