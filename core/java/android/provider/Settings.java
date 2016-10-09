2fc8837488f70dfef60700f9fcf82f6dabc96249
Davor Bertovic <davor@losinj.com>
2016-10-09 14:17:55 +0200

    FWB: Battery Bar [1/2]
    * Changes:
      Battery bar enhancements [1/2]
      Have Battery bar controller obey Hide value
      BatteryBar [1/2]
    
    Change-Id: If8e8eee26b20f63a813c476b35048967183f8ab3
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3725,12 +3725,41 @@ public final class Settings {
          */
         public static final String POWERMENU_SCREENRECORD = "powermenu_screenrecord";
 
-
-        /**
-         * Disable dashboard suggestions in settings
+	/**
+         * Whether to show the battery bar
          * @hide
          */
-        public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_CHARGING_COLOR = "statusbar_battery_bar_charging_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_BATTERY_LOW_COLOR = "statusbar_battery_bar_battery_low_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
 
 	/**
           * Volume keys control cursor in text fields (default is 0)
