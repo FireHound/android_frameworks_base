0d01e730de5d00cfd3b9cfb5ba1b89b7e08325c5
bigrushdog <randall.rushing@gmail.com>
2016-10-15 14:08:19 -0700

    Recents: Don't show alternate recents when task is locked
    Simulate AOSP behavior and don't show any recent task managers
    when screenpinning is on. This solution works and is simple.
    However, a proper solution would involve handling all this
    in the activity stack management, as is the case with AOSP recents.
    
    Change-Id: I957659d6ef251e532b55eb7dfca8e0f06ff51b49

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1590,6 +1590,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     @Override
     public void screenPinningStateChanged(boolean enabled) {
+        super.screenPinningStateChanged(enabled);
         mNavigationController.screenPinningStateChanged(enabled);
     }
 
