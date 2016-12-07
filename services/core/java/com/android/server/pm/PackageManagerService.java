53b60553b9cd71e2e98fb2b01e4b784fe0121885
LorDClockaN <lordclockan@gmail.com>
2016-12-07 20:44:57 +0100

    Fix build after OMS
    Change-Id: I07da09d3c2f2f62e8b9fcd565f6dcd6a0a0b5601

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -21357,6 +21357,7 @@ Slog.v(TAG, ":: stepped forward, applying functor at tag " + parser.getName());
             synchronized (mPackages) {
                 return mSettings.wasPackageEverLaunchedLPr(packageName, userId);
             }
+        }
 
         @Override
         public List<PackageInfo> getOverlayPackages(int userId) {
