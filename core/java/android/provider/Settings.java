111ff6319579a6dde92e54117e6ad89a7f161256
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-12-10 16:28:19 +0100

    [2/2] frameworks: show charging current & voltage on lockscreen
    Thanx to ezio84 for the switch ;)
    
    Change-Id: I0a49bb204684a178f4977ea368b2970f2e5ec5ad
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4192,6 +4192,12 @@ public final class Settings {
          */
         public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
 
+         /**
+          * Whether to show the negociated charger current in the lockscreen
+          * @hide
+          */
+         public static final String LOCKSCREEN_CHARGING_CURRENT = "lockscreen_charging_current";
+ 
         /**
          * location of the clear all rectents button
          * @hide
