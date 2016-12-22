2993e01c8a659c7cec4ba914ea2e27ac958fbcea
PMS22 <prathams99@rediff.com>
2016-12-22 08:15:28 +0000

    EasterEgg: Show a toast when cat is captured
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- packages/EasterEgg/src/com/android/egg/neko/Cat.java
+++ packages/EasterEgg/src/com/android/egg/neko/Cat.java
@@ -23,6 +23,7 @@ import android.graphics.*;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Random;
@@ -201,6 +202,7 @@ public class Cat extends Drawable {
     }
 
     public static Cat create(Context context) {
+    Toast.makeText(context, R.string.cat_captured, Toast.LENGTH_SHORT).show();
         return new Cat(context, Math.abs(ThreadLocalRandom.current().nextInt()));
     }
 
