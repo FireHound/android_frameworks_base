7efb5430086df24455d16f54b6438cbc12c89770
Willi Ye <williye97@gmail.com>
2017-01-09 21:24:30 +0100

    Fwb: Port PA battery circle style [2/3]
    Change-Id: I9efea4ceffe407e73205802fe504f4a553d11132
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -75,6 +75,7 @@ public class BatteryMeterDrawable extends Drawable implements
     public static final int BATTERY_STYLE_HIDDEN    = 4;
     public static final int BATTERY_STYLE_LANDSCAPE = 5;
     public static final int BATTERY_STYLE_TEXT      = 6;
+    public static final int BATTERY_STYLE_SOLID     = 7;
 
     private final int[] mColors;
     private final int mIntrinsicWidth;
@@ -563,6 +564,8 @@ public class BatteryMeterDrawable extends Drawable implements
                 return R.drawable.ic_battery_circle;
             case BATTERY_STYLE_PORTRAIT:
                 return R.drawable.ic_battery_portrait;
+           case BATTERY_STYLE_SOLID:
+                return R.drawable.ic_battery_solid; 
             default:
                 return 0;
         }
@@ -576,6 +579,8 @@ public class BatteryMeterDrawable extends Drawable implements
                 return R.style.BatteryMeterViewDrawable_Circle;
             case BATTERY_STYLE_PORTRAIT:
                 return R.style.BatteryMeterViewDrawable_Portrait;
+            case BATTERY_STYLE_SOLID:
+                return R.style.BatteryMeterViewDrawable_Solid;
             default:
                 return R.style.BatteryMeterViewDrawable;
         }
