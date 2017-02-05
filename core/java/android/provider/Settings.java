77da09431d92ec409b0d415cde4336df5ef50ff2
Pranav Vashi <neobuddy89@gmail.com>
2017-02-05 12:48:46 +0530

    Make Data/Wifi activity icon in statusbar optional [1/2]
    * Enabled by default - AOSP behavior.
    * Using Tuner API.
    * Rewrite based on https://github.com/DirtyUnicorns/android_frameworks_base/commit/f092cc1bfa6feadb86eb1a29c20843ec63d67c3f
    
    Change-Id: I629b0d2cd0422409ffb3d0a4b58b80329addb077

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4440,6 +4440,12 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
+         * Whether to display data activity arrows for wifi/data connections
+         * @hide
+         */
+        public static final String DATA_ACTIVITY_ARROWS = "data_activity_arrows";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
