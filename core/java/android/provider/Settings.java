e82388c083d3ed454e89ed5607966d03abd6c4ee
PMS22 <prathams99@rediff.com>
2017-02-28 14:46:59 +0000

    Hide Emergency Button on  Lockscreen [1/2]
    FireHound Edits :-
    - Adapt for O
    - Eats some space
    - Removing it makes lockscreen look clean
    - During times of "Emergency", we all do nothing better that booting back to TWRP.
    
    *DU edits/notes*
    
    - Hiding the emergency button is something that some users wish to have.
    I don't get it but it can be done while keeping the stock behavior by default
    
    - Removed unnecessary imports
    - Removed unnecessary void used in the original patch
    - Kept the original argument for 'IsInCall' which was removed in original patch
    
    Honestly this was a train wreck when we got it but it works good now :-)
    
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4356,6 +4356,12 @@ public final class Settings {
                 "statusbar_battery_bar_blend_color_reverse";
 
         /**
+         * Show/Hide Emergency Button on Lockscreen
+         * @hide
+         */
+        public static final String SHOW_EMERGENCY_BUTTON = "show_emergency_button";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
