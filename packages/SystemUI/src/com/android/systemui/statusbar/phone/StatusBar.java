415ea338fdef9ac55432d097a6e5f11927b129ad
maxwen <max.weninger@gmail.com>
2017-08-30 11:24:23 +0200

    [1/2] base: add back qs header images
    AICPfied:
     * added another utility function
     * removed elevation and background attributes to adjust to 8.1
    
    Change-Id: Id9ab770f1c90f609c121b4121c4633baa274149b
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -200,6 +200,7 @@ import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.keyguard.ScreenLifecycle;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
 import com.android.systemui.navigation.Navigator;
+import com.android.systemui.omni.StatusBarHeaderMachine;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.qs.QS;
 import com.android.systemui.plugins.statusbar.NotificationMenuRowPlugin.MenuItem;
@@ -969,6 +970,8 @@ public class StatusBar extends SystemUI implements DemoMode,
     private NavigationBarFragment mNavigationBar;
     private View mNavigationBarView;
 
+    private StatusBarHeaderMachine mStatusBarHeaderMachine;
+
     @Override
     public void start() {
         mNetworkController = Dependency.get(NetworkController.class);
@@ -1388,6 +1391,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             createUserSwitcher();
         }
 
+        mStatusBarHeaderMachine = new StatusBarHeaderMachine(mContext);
+
         // Set up the quick settings tile panel
         View container = mStatusBarWindow.findViewById(R.id.qs_frame);
         if (container != null) {
@@ -1412,6 +1417,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                     mKeyguardStatusBar.setQSPanel(mQSPanel);
                     mQuickStatusBarHeader = ((QSFragment) qs).getQsHeader();
                     mQuickStatusBarHeader = ((QSFragment) qs).getQuickStatusBarHeader();
+                    mStatusBarHeaderMachine.addObserver(mQuickStatusBarHeader);
+                    mStatusBarHeaderMachine.updateEnablement();
                 }
             });
         }
