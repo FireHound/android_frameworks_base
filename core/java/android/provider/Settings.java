c2ab1961814d3c1adeada8bd553a54e191497332
ezio84 <brabus84@gmail.com>
2017-09-06 21:32:08 +0200

    QS: Add partial/full screenshot QS tile
    single tap to choose the mode, long press to fire
    
    thanks kantjer for the icons!
    
      From: maxwen <max.weninger@gmail.com>
      Date: Mon, 8 Jan 2018 02:52:58 +0100
      Subject: base: persist screenshot type in settings
    
      save state set in qs tile into settings

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5017,6 +5017,13 @@ public final class Settings {
         public static final String WIRELESS_CHARGING_ANIMATION = "wireless_charging_animation";
 
         /**
+         * 0 - fullscreen
+         * 1 - partial
+         * @hide
+         */
+        public static final String SCREENSHOT_DEFAULT_MODE = "screenshot_default_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
