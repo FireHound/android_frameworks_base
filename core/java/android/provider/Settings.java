ab8e40f06784ad036bb6ea8824373a3b70fc20d1
LorDClockaN <lordclockan@gmail.com>
2017-10-11 14:30:25 +0200

    FWB: Toast icon switch [1/2]
    Change-Id: I59ffd10f20a2ae9491f76380ec9af641e0844928
    
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4392,6 +4392,12 @@ public final class Settings {
         public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
         /**
+         * Toast icon switch
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
