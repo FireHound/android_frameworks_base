df02b1cc7055b6f8ffd43f0497133f4a9301d6b5
PMS22 <prathams99@rediff.com>
2018-01-08 17:24:21 +0000

    VolumeDialog: Force expanded volume panel [1/2]
    - Based on :-
    https://github.com/CyanideL/android_frameworks_base/commit/e03e3bc1c56ed5149192f9b52bfeb9714eebfc3f#diff-b079663de976cf5d60c876e5684f5756
    
    FireHound Edits :-
    - Update for oreo 8.1
    
    TO-DO :-
    - To hide the expand button in volume panel
      when panel is force expanded
    
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4355,6 +4355,12 @@ public final class Settings {
         public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
 
         /**
+         * Force Volume panel in expanded mode
+         * @hide
+         */
+        public static final String VOLUME_DIALOG_FORCE_EXPANDED = "volume_dialog_force_expanded";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
