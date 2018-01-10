aed83a73a5c21dafe6cc0c256cf8bd6d636e0ec6
faust93 <monumentum@gmail.com>
2018-01-10 13:22:05 +0200

    Add @hide to fix build

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4154,6 +4154,7 @@ public final class Settings {
          * 1 = Statusbar only
          * 2 = Navbar only
          * 3 = Fullscreen
+         * @hide
          */
         public static final String IMMERSIVE_RECENTS = "immersive_recents";
 
