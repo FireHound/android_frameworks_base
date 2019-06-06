285dce1cbbbc1d892875598b74c8a734cb3368d4
Anushek Prasal <anushekprasal@gmail.com>
2019-06-06 14:18:24 +0530

    base: Add toggle to disable charging animation [1/2]
    Signed-off-by: Shubh@m <shubhindia123@gmail.com>
    Change-Id: Idc82ddbe75f905260f7634d8feccc1adbc71298b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5011,6 +5011,12 @@ public final class Settings {
         public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
 
         /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
