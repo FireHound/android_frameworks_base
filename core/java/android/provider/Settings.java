db760db6453edef64f60f0a41a11b40417b281df
00day0 <therandomuser11@gmail.com>
2020-01-07 20:15:27 +0100

    base: Blur behind quick settings [1/2]
    @neobuddy89: Code and logic improvements.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: PMS22 <prathams99@rediff.com>
    Change-Id: Id88cf830afd7ee6ce1d2a3579647ff139071bd93

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5097,6 +5097,12 @@ public final class Settings {
         public static final String STABILIZATION_VELOCITY_AMPLITUDE = "stabilization_velocity_amplitude";
 
         /**
+         * Whether to blur behind QS
+         * @hide
+         */
+        public static final String QS_BACKGROUND_BLUR = "qs_background_blur";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
