violet-reborn/android_frameworks_base-1

Hide power menu on secure lockscreen - improvements
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1477,8 +1477,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             case LONG_PRESS_POWER_GLOBAL_ACTIONS:
                 mPowerKeyHandled = true;
-                performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
-                        "Power - Long Press - Global Actions");
                 KeyguardManager km = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
                 boolean locked = km.inKeyguardRestrictedInputMode();
                 boolean globalActionsOnLockScreen = Settings.Global.getInt(
@@ -1486,6 +1484,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 if (locked && !globalActionsOnLockScreen) {
                     behavior = LONG_PRESS_POWER_NOTHING;
                 } else {
+                    performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
+                        "Power - Long Press - Global Actions");
                     showGlobalActionsInternal();
                 }
                 break;
@@ -1533,14 +1533,20 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void powerVeryLongPress() {
         switch (mVeryLongPressOnPowerBehavior) {
-        case VERY_LONG_PRESS_POWER_NOTHING:
-            break;
-        case VERY_LONG_PRESS_POWER_GLOBAL_ACTIONS:
-            mPowerKeyHandled = true;
-            performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
-                    "Power - Very Long Press - Show Global Actions");
-            showGlobalActionsInternal();
-            break;
+            case VERY_LONG_PRESS_POWER_NOTHING:
+                break;
+            case VERY_LONG_PRESS_POWER_GLOBAL_ACTIONS:
+                mPowerKeyHandled = true;
+                KeyguardManager km = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
+                boolean locked = km.inKeyguardRestrictedInputMode();
+                boolean globalActionsOnLockScreen = Settings.Global.getInt(
+                        mContext.getContentResolver(), Settings.Global.LOCKSCREEN_ENABLE_POWER_MENU, 1) != 0;
+                if (!locked || globalActionsOnLockScreen) {
+                    performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
+                        "Power - Very Long Press - Show Global Actions");
+                    showGlobalActionsInternal();
+                }
+                break;
         }
     }
 

FireHound/android_frameworks_base

Change-Id: I6faf7154904e0edcbfc089b9fb813faf4c647387

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1487,8 +1487,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             case LONG_PRESS_POWER_GLOBAL_ACTIONS:
                 mPowerKeyHandled = true;
-                performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
-                        "Power - Long Press - Global Actions");
                 KeyguardManager km = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
                 boolean locked = km.inKeyguardRestrictedInputMode();
                 boolean globalActionsOnLockScreen = Settings.System.getInt(
@@ -1496,6 +1494,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 if (locked && !globalActionsOnLockScreen) {
                     behavior = LONG_PRESS_POWER_NOTHING;
                 } else {
+                    performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
+                        "Power - Long Press - Global Actions");
                     showGlobalActionsInternal();
                 }
                 break;
@@ -1543,14 +1543,20 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void powerVeryLongPress() {
         switch (mVeryLongPressOnPowerBehavior) {
-        case VERY_LONG_PRESS_POWER_NOTHING:
-            break;
-        case VERY_LONG_PRESS_POWER_GLOBAL_ACTIONS:
-            mPowerKeyHandled = true;
-            performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
-                    "Power - Very Long Press - Show Global Actions");
-            showGlobalActionsInternal();
-            break;
+            case VERY_LONG_PRESS_POWER_NOTHING:
+                break;
+            case VERY_LONG_PRESS_POWER_GLOBAL_ACTIONS:
+                mPowerKeyHandled = true;
+                KeyguardManager km = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
+                boolean locked = km.inKeyguardRestrictedInputMode();
+                boolean globalActionsOnLockScreen = Settings.System.getInt(
+                        mContext.getContentResolver(), Settings.System.LOCKSCREEN_ENABLE_POWER_MENU, 1) != 0;
+                if (!locked || globalActionsOnLockScreen) {
+                    performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
+                        "Power - Very Long Press - Show Global Actions");
+                    showGlobalActionsInternal();
+                }
+                break;
         }
     }
 