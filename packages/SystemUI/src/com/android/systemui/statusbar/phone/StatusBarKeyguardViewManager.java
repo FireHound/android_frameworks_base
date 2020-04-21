2f05318cb75fb13adc356135cdda6ffb225bf110
PMS22 <prathams99@rediff.com>
2020-04-21 05:21:50 +0000

    Android_R Notifications: Increase width in between headers
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -235,7 +235,7 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         mBouncer = SystemUIFactory.getInstance().createKeyguardBouncer(mContext,
                 mViewMediatorCallback, mLockPatternUtils, container, dismissCallbackRegistry,
                 mExpansionCallback, falsingManager, bypassController);
-        mExpansionCallback, falsingManager, bypassController);
+        mBouncer.setUnlockWithoutBouncer(mUnlockWithoutBouncer);
         mNotificationPanelView = notificationPanelView;
         notificationPanelView.addExpansionListener(this);
         mBypassController = bypassController;
